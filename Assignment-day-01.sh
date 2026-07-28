b0d9cef0e6542c08b590200457e0ae322bf92e91
anum asif <anum.asif85aa@gmail.com>
2026-07-28 23:34:54 +0500

    Day-01 assignment final

---
+++ Assignment-day-01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "========================================="
+echo "Assignment Day-01"
+echo "Name: Anum"
+echo "Topic: Shell Scripting Basics"
+echo "==============================="
+what is Shell?
+Shell is a command interpreter between user and operating system.
+what is bash?
+Bash is a Bourne Again Shell,its a Linux shell which is a command language.
+:wq
